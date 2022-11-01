98a5b71b4bb4ba0011caaa5c6cb2704b60fd2ed6
Yuriy Kormin <yuriy.kormin@gmail.com>
2022-11-01 13:10:36 +0400

    add / to urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task_manager import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index)
 ]
