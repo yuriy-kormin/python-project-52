9234c76bdf5ab727e8896ad4961dde6f5adc5338
Yuriy Kormin <yuriy.kormin@gmail.com>
2022-11-15 19:06:28 +0400

    modify user avtorization to encrypt pass

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,18 +1,97 @@
 from django import forms
 # from .models import User
+# from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
-class UserForm(forms.ModelForm):
+# class UserForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = ('first_name',
+#                   'last_name',
+#                   'username',
+#                   'password'
+#                   )
+#         widgets = {
+#            # 'username': forms.TextInput(attrs={'placeholder': 'username'}),
+#             'password': forms.TextInput(),
+#         # .widget.attrs['placeholder'] = 'username'
+#         }
+#
+#     def save(self, commit=True):
+#         user = super().save(commit=False)
+#         user.set_password(self.cleaned_data['password'])
+#         if commit:
+#             user.save()
+#         return user
+#
+from django.http import HttpResponse
+
+
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
     class Meta:
         model = User
         fields = ('first_name',
                   'last_name',
                   'username',
-                  'password'
                   )
-        widgets = {
-           # 'username': forms.TextInput(attrs={'placeholder': 'username'}),
-            'password': forms.PasswordInput(),
-        # .widget.attrs['placeholder'] = 'username'
-        }
+        localized_fields = ('__all__')
+
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserUpdateForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+
+    class Meta:
+        model = User
+        fields = ('first_name',
+                  'last_name',
+                  'username',
+                  # 'password'
+                  )
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            # messages.info(request, 'User not exist')
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,25 +1,36 @@
+from django.contrib.auth.mixins import AccessMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponse
 from django.views import View
 from django.views.generic import ListView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.auth.models import User
-from .forms import UserForm
+from .forms import UserCreationForm, LoginForm, UserUpdateForm
 
 
 class UserListView(ListView):
+
     model = User
     template_name = "users/list.html"
 
 
-class UserCreateView(CreateView):
-    form_class = UserForm
+class UserCreateView(SuccessMessageMixin,AccessMixin, CreateView):
+    form_class = UserCreationForm
     template_name = 'users/create.html'
     success_url = '/'
+    success_message = 'User created succesfully'
+    permission_denied_message = " error"
+
+    # def get(self, request,*args,**kwargs):
 
 
-class UserUpdateView(UpdateView):
+class UserUpdateView(SuccessMessageMixin,AccessMixin, UpdateView):
     model = User
-    form_class = UserForm
+    form_class = UserUpdateForm
     template_name = 'users/edit.html'
+    success_message = 'Data was updated'
+    permission_denied_message = 'second error'
     success_url = '/'
 
 
@@ -29,9 +40,15 @@ class UserDeleteView(DeleteView):
     success_url = '/'
 
 
-class UserLoginView(View):
-    def get(self, request):
-        pass
+class UserLoginView(SuccessMessageMixin, LoginView):
+    # authentication_form = LoginForm
+    success_message = 'You are logged in'
+    template_name = 'users/login.html'
+    next_page = '/'
+    # next_page = '/'
+    #
+    # def get(self, request):
+    #     pass
 
 
 class UserLogoutView(View):
