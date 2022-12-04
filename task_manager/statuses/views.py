359e8e7e0264de7a0654b6d1a0dd9b4536f168a6
yuriy.kormin <yuriy.kormin@gmail.com>
2022-12-05 01:02:30 +0400

    modify Status app: add views

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from task_manager.statuses.models import Status
 
-# Create your views here.
+
+class StatusListView(ListView):
+    model = Status
+    template_name = "statuses/list.html"
+
+
+class StatusCreateView(CreateView):
+    model = Status
+    template_name = "statuses/create.html"
+
+
+class StatusUpdateView(UpdateView):
+    model = Status
+    template_name = "statuses/update.html"
+
+class StatusDeleteView(DeleteView):
+    model = Status
+    template_name = "statuses/delete.html"
+
