2eee25ef81fc2ee367c046af4a139ff771ab75e2
yuriy.kormin <yuriy.kormin@gmail.com>
2022-12-05 01:03:04 +0400

    modify Status app: modify models.py: add max_length = 400

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Status(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
-    name = models.CharField()
+    name = models.CharField(max_length=400)
