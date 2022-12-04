ac9cc55df2f276f50bf93a0c72b3d8d8670836a6
yuriy.kormin <yuriy.kormin@gmail.com>
2022-12-05 01:31:11 +0400

    modify Statuses app: add list view and translations

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,11 +1,17 @@
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from task_manager.statuses.models import Status
-
+from django.utils.translation import gettext_lazy as _
 
 class StatusListView(ListView):
     model = Status
     template_name = "statuses/list.html"
+    extra_context = {
+        'statuses': _('statuses'),
+        'ID': _('ID'),
+        'name': _('name'),
+        'created_at': _('created at'),
+    }
 
 
 class StatusCreateView(CreateView):
