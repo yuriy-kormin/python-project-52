90a5cde2865346d085fe035736097b55ddc4cc8c
yuriy.kormin <yuriy.kormin@gmail.com>
2022-12-05 00:51:33 +0400

    modify Status app: add app name to apps.py

--- task_manager/statuses/apps.py
+++ task_manager/statuses/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class StatusesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'statuses'
+    name = 'task_manager.statuses'
