51af86c54412f36630d6c77cf3a7a906f5f565b5
yuriy.kormin <yuriy.kormin@gmail.com>
2022-12-06 10:28:03 +0400

    task app: create model

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from task_manager.statuses.models import Status
 
-# Create your models here.
+
+class Task(models.Model):
+    date_joined = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=400)
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.PROTECT,
+                               related_name='author')
+    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    performer = models.ForeignKey(User, on_delete=models.PROTECT,
+                                  related_name='performer')
