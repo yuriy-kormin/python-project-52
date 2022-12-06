0c7a594fe4b806bce6fea05e065569a6ba55b78c
yuriy.kormin <yuriy.kormin@gmail.com>
2022-12-06 10:29:07 +0400

    task app: add pass views

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import TaskCreateView, TaskListView, \
-    TaskUpdateView, TaskDeleteView
+    TaskUpdateView, TaskDeleteView, TaskView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task_list'),
     path('create/', TaskCreateView.as_view(), name='task_add'),
     path('<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
     path('<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
-    path('<int:pk>/', TaskDeleteView.as_view(), name='task_view'),
+    path('<int:pk>/', TaskView.as_view(), name='task_view'),
 ]
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import CreateView, UpdateView, \
+    DeleteView, DetailView
+from django.views.generic import ListView
+
+
+class TaskCreateView(CreateView):
+    pass
+
+
+class TaskListView(ListView):
+    pass
+
+
+class TaskUpdateView(UpdateView):
+    pass
+
+
+class TaskDeleteView(DeleteView):
+    pass
+
+
+class TaskView(DetailView):
+    pass
