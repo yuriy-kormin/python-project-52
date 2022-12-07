e8c37ba19d93ffa0664c954d6b72f61309746af9
yuriy.kormin <yuriy.kormin@gmail.com>
2022-12-07 10:04:55 +0400

    task app: add test update

---
+++ task_manager/tasks/tests/test_update.py
@@ -0,0 +1,37 @@
+from task_manager.users.models import TaskUser as User
+from django.urls import reverse_lazy as reverse
+from django.test import TransactionTestCase
+from ..models import Status, Task
+
+
+class UpdateTask(TransactionTestCase):
+    fixtures = ['db_task.json']
+
+    def test_update_open_without_login(self):
+        response = self.client.get(reverse('task_update', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 302)
+        response = self.client.post(reverse('task_update', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 302)
+
+    def test_update_task(self):
+        user = User.objects.all().first()
+        self.client.force_login(user=user)
+        response = self.client.get(reverse('task_update', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 200)
+        task = Task.objects.all().first()
+        status = Status.objects.all().first()
+        user2 = User.objects.create_user(username='test', password='testpass')
+        task2 = {
+            'name': 'test_task',
+            'status': status.id,
+            'performer': user2.id,
+        }
+        response = self.client.post(
+            reverse('task_update', kwargs={'pk': task.id}),
+            task2
+        )
+
+        self.assertEqual(response.status_code, 302)
+        task = Task.objects.get(pk=task.id)
+        self.assertEqual(task.name, task2['name'])
+        self.assertEqual(task.performer_id, task2['performer'])
