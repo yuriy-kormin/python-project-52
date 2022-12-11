fecf41b7816962434910a790098a73358cb31cd4
yuriy.kormin <yuriy.kormin@gmail.com>
2022-12-11 18:49:06 +0400

    add default django secret

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,7 +31,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET')
+SECRET_KEY = 'jJ3iFu3yeKZnY9D9GzTq52SP46vA1m6KBOhXqF4U'
+
+SECRET_KEY_ENV = os.environ.get('DJANGO_SECRET')
+if SECRET_KEY_ENV:
+    SECRET_KEY = SECRET_KEY_ENV
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- task_manager/tasks/tests/test_select.py
+++ task_manager/tasks/tests/test_select.py
@@ -1,7 +1,4 @@
-from task_manager.users.models import TaskUser as User
-from django.urls import reverse_lazy as reverse
 from django.test import TestCase
-from ..models import Status, Task
 
 
 class SelectStatus(TestCase):
