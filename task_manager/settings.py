f055714746441a7551ddc05fa9b04c172f802088
yuriy.kormin <yuriy.kormin@gmail.com>
2022-12-11 21:02:15 +0400

    add ru-ru

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -133,7 +133,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 # LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
