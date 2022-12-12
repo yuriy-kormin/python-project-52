38c1012bd3b342b24bbb02d1515537c60f6f337b
yuriy.kormin <yuriy.kormin@gmail.com>
2022-12-12 16:54:25 +0400

    update urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name='root'),
     path('users/', include('task_manager.users.urls')),
     path('statuses/', include('task_manager.statuses.urls')),
-    path('marks/', include('task_manager.marks.urls')),
+    path('labels/', include('task_manager.marks.urls')),
     path('tasks/', include('task_manager.tasks.urls')),
     path(
         'login/', UserLoginView.as_view(),
