c9d55773167196f37c2b1bd4613b318b53c12270
yuriy.kormin <yuriy.kormin@gmail.com>
2022-12-23 14:33:39 +0400

    write mixins:  LoginRequiredCustomMixin  DeleteProtectedErrorMixin

---
+++ task_manager/mixins.py
@@ -0,0 +1,29 @@
+from django.contrib import messages
+from django.db.models import ProtectedError
+from django.shortcuts import redirect
+from django.views.generic.edit import DeletionMixin
+
+
+class LoginRequiredCustomMixin:
+    permission_denied_message = ''
+    login_url = ''
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            messages.error(self.request, self.permission_denied_message)
+            return redirect(self.login_url)
+        return super().dispatch(request, *args, **kwargs)
+
+
+class DeleteProtectErrorMixin(DeletionMixin):
+    protected_error_message = ''
+    success_message = ''
+
+    def form_valid(self, form):
+        try:
+            super().delete(self.request)
+        except ProtectedError:
+            messages.error(self.request, self.protected_error_message)
+        else:
+            messages.info(self.request, self.success_message)
+        return redirect(self.get_success_url())
