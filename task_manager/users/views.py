a670d8934bcda77adb019fe508e8c613726af1e0
yuriy.kormin <yuriy.kormin@gmail.com>
2022-12-23 14:35:31 +0400

    modify user app to use mixin in separate files

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,14 +1,12 @@
-from django.contrib import messages
-from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.db.models import ProtectedError
+from .mixins import UserTestCustomMixin
 from .models import TaskUser as User
-from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .forms import UserForm
+from task_manager.mixins import LoginRequiredCustomMixin, DeleteProtectErrorMixin
 
 
 class UserListView(ListView):
@@ -34,16 +32,9 @@ class UserCreateView(SuccessMessageMixin, CreateView):
     }
     success_message = _('User created successfully')
 
-    def form_invalid(self, form):
-        for field in form.fields:
-            if field in form.errors:
-                form[field].field.widget.attrs['class'] += ' is-invalid '
-            else:
-                form[field].field.widget.attrs['class'] += ' is-valid '
-        return super().form_invalid(form)
 
-
-class UserUpdateView(UserPassesTestMixin, UpdateView):
+class UserUpdateView(LoginRequiredCustomMixin, UserTestCustomMixin,
+                     SuccessMessageMixin, UpdateView):
     model = User
     form_class = UserForm
     template_name = 'users/create.html'
@@ -53,65 +44,25 @@ class UserUpdateView(UserPassesTestMixin, UpdateView):
         'header': _('Edit user'),
         'button_title': _('Update'),
     }
-
-    def form_valid(self, form):
-        messages.success(self.request, _('User update successfully'))
-        return super().form_valid(form)
-
-    def test_func(self):
-        user = self.request.user
-        if user.is_authenticated:
-            user_instance = User.objects.get(pk=user.id)
-            obj = self.get_object()
-            object_instance = User.objects.get(pk=obj.id)
-            if user_instance == object_instance:
-                return True
-            messages.error(self.request, _('You cannot edit another user'))
-        else:
-            messages.error(self.request, _('Please login to modify user'))
-        return False
-
-    def handle_no_permission(self):
-        if self.request.user.is_authenticated:
-            return redirect(self.success_url)
-        return redirect(self.login_url)
+    raise_exception = False
+    permission_denied_message = _('Please login to modify user')
+    modify_error_message = _('You cannot edit another user')
+    success_message = _('User update successfully')
 
 
-class UserDeleteView(UserPassesTestMixin, DeleteView):
+class UserDeleteView(LoginRequiredCustomMixin, UserTestCustomMixin,
+                     DeleteProtectErrorMixin, DeleteView):
     model = User
     template_name = 'users/delete.html'
     success_url = reverse_lazy('user_list')
+    login_url = reverse_lazy('user_login')
     extra_context = {
         'header': _('Remove user'),
         'button_title': _('Yes, remove'),
         'message': _('Are you sure delete'),
     }
     raise_exception = False
-
-    def test_func(self):
-        user = self.request.user
-        if user.is_authenticated:
-            user_instance = User.objects.get(pk=user.id)
-            obj = self.get_object()
-            object_instance = User.objects.get(pk=obj.id)
-            if user_instance == object_instance:
-                return True
-            messages.error(self.request, _('You cannot delete another user'))
-        else:
-            messages.error(self.request, _('Please login to delete user'))
-        return False
-
-    def handle_no_permission(self):
-        if self.request.user.is_authenticated:
-            return redirect(self.success_url)
-        return redirect(reverse_lazy('user_login'))
-
-    def form_valid(self, form):
-        try:
-            self.object.delete()
-        except ProtectedError:
-            messages.error(self.request, _(
-                'User can\'t be deleted - on use now'))
-        else:
-            messages.info(self.request, _('User was successfully deleted'))
-        return redirect(self.get_success_url())
+    permission_denied_message = _('Please login to delete user')
+    modify_error_message = _('You cannot delete another user')
+    success_message = _('User was successfully deleted')
+    protected_error_message = _('User can\'t be deleted - on use now')
