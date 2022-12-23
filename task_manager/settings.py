d88ea722ad4787f6f14b0cac061d56bb9b3d90ac
yuriy.kormin <yuriy.kormin@gmail.com>
2022-12-23 16:14:50 +0400

    add default django-secret

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'jJ3iFu3yeKZnY9D9GzTq52SP46vA1m6KBOhXqF4U'
+SECRET_KEY = 'jJ3iFu3yeKZnY9D9GzTq52SP46vA1m6KBOhXqF4U'
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
